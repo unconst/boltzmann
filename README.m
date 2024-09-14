a6180038ef7b06bb540cbc4ca7d26ba43541e8ee
const <jake@bittensor.com>
2024-09-14 21:34:37 +0000

    readme

---
+++ README.m
@@ -0,0 +1,27 @@
+]3 | _\~ ~|~ /? () 
+                                        
+---
+BISTRO – Bittensor Incentivized and Scalable Training with Reward Optimization. 
+---
+
+# Step 1.
+  - Create an S3 <Bucket> on AWS and add export your AWS API Key.
+  - `export AWS_SECRET_ACCESS_KEY=`
+  - `export AWS_ACCESS_KEY_ID=`
+
+# Step 2.
+  - Install python3 requirements.
+  - `python3 -m pip install -r requirements.txt`
+
+# Step 3. 
+  - Register your miner on subnet 212 on testnet.
+  - `btcli s register --wallet.name <> --wallet.hotkey <> --subtensor.network test --netuid 212`
+
+# Step 4.
+  - Run your miner.
+  - `python3 miner.py --wallet.name <> --wallet.hotkey <> --subtensor.network test --netuid 212 --bucket <Bucket>`
+
+# Step 5.
+  - Run your validator.
+  - `python3 validator.py --wallet.name <> --wallet.hotkey <> --subtensor.network test --netuid 212 --bucket <Bucket>`
+
