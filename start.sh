5cd00b8414a0aa539d900c2156a8c05ca000cc0e
const <jake@bittensor.com>
2024-09-19 21:49:09 +0000

    r

--- start.sh
+++ start.sh
@@ -27,9 +27,4 @@ pm2 start miner.py --interpreter python3 --name M3 -- --wallet.name Alice --wall
 pm2 start miner.py --interpreter python3 --name M4 -- --wallet.name Alice --wallet.hotkey Eve --subtensor.network test --device cuda:5 --use_wandb
 
 
-# 
-# python3 validator.py --wallet.name Alice --wallet.hotkey Alice --device cuda:0 --restart
-# python3 miner.py --wallet.name Alice --wallet.hotkey Bob --device cuda:1 
-
-
 
