66e301af5f279af28f0251f961e4e61e1fe072c9
Samuel Dare <sam@tunnelvisionlabs.xyz>
2024-09-22 23:18:07 +0400

    feat: download hyperparameters from gist

--- hparams.py
+++ hparams.py
@@ -14,41 +14,33 @@
 # THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION
 # OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
 # DEALINGS IN THE SOFTWARE.
-
+import json
+import requests
 from types import SimpleNamespace
 from transformers import AutoTokenizer, LlamaConfig
 
+
 def load_hparams() -> SimpleNamespace:
-    hparams = {
-        # Steps between full state syncing
-        'epoch_length': 25000,
-        # Delta compression rate.
-        'compression': 300,
-        # Global sequence length
-        'sequence_length': 2048,
-        # AutoTokenizer name.
-        'tokenizer_name': 'gpt2',
-        # Model arch.
-        'num_hidden_layers': 16,         # Layers
-        'hidden_size': 2048,             # Hidden Size
-        'intermediate_size': 8192,       # Intermediate Size
-        'num_attention_heads': 8,        # Attention Heads
-        'num_key_value_heads': 8,        # Key/Value Heads
-        'activation_function': "swiGLU", # Activation Function
-        'max_position_embeddings': 2048, # Positional Embeddings (RoPE)
-    }
+    # URL of the gist containing the JSON file
+    gist_url = "https://gist.githubusercontent.com/distributedstatemachine/75e8db446d2f1eaf1417f06d55765a32/raw/hprams.json"
+    # Fetch the JSON data from the gist
+    response = requests.get(gist_url)
+    response.raise_for_status()  # Raise an exception for HTTP errors
+    hparams = json.loads(response.text)
     # Convert the dictionary to a SimpleNamespace
     hparams_ns = SimpleNamespace(**hparams)
-    hparams_ns.tokenizer = AutoTokenizer.from_pretrained( hparams_ns.tokenizer_name, verbose=False, clean_up_tokenization_spaces=True )
+    hparams_ns.tokenizer = AutoTokenizer.from_pretrained(
+        hparams_ns.tokenizer_name, verbose=False, clean_up_tokenization_spaces=True
+    )
     hparams_ns.tokenizer.pad_token = hparams_ns.tokenizer.eos_token
     hparams_ns.model_config = LlamaConfig(
-        vocab_size = hparams_ns.tokenizer.vocab_size,
-        hidden_size = hparams_ns.hidden_size,
-        num_hidden_layers = hparams_ns.num_hidden_layers,
-        num_attention_heads = hparams_ns.num_attention_heads,
-        intermediate_size = hparams_ns.intermediate_size,
-        num_key_value_heads = hparams_ns.num_key_value_heads,
-        activation_function = hparams_ns.activation_function,
-        max_position_embeddings = hparams_ns.max_position_embeddings,
+        vocab_size=hparams_ns.tokenizer.vocab_size,
+        hidden_size=hparams_ns.hidden_size,
+        num_hidden_layers=hparams_ns.num_hidden_layers,
+        num_attention_heads=hparams_ns.num_attention_heads,
+        intermediate_size=hparams_ns.intermediate_size,
+        num_key_value_heads=hparams_ns.num_key_value_heads,
+        activation_function=hparams_ns.activation_function,
+        max_position_embeddings=hparams_ns.max_position_embeddings,
     )
     return hparams_ns
